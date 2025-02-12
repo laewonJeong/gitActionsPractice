817793064ae03e2aa148d002cf5774c3435ad441
래원🌊 <81546637+laewonJeong@users.noreply.github.com>
2025-02-12 20:55:17 +0900

    update: App.java

--- app/src/main/java/gitactionspractice/App.java
+++ app/src/main/java/gitactionspractice/App.java
@@ -12,6 +12,6 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println("Factorial of 5: " + factorial(5));
+        System.out.println("Factorial of 6: " + factorial(6));
     }
 }
