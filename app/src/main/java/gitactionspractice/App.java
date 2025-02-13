875c0c85d3e26367affb818a619ec20576900587
lawonjeong <laewonjeong@kangwon.ac.kr>
2025-02-13 21:48:29 +0900

    update: App.java

--- app/src/main/java/gitactionspractice/App.java
+++ app/src/main/java/gitactionspractice/App.java
@@ -2,9 +2,9 @@ package gitactionspractice;
 
 public class App {
     public static int factorial(int n) {
-        if (n < 0) {
+        /*if (n < 0) {
             throw new IllegalArgumentException("n must be >= 0");
-        }
+        }*/
         if (n == 0 || n == 1) {
             return 1;
         }
